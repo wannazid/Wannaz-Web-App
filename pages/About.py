72babb5a4d10c8c15f8a6f4e6318b7eaae1bc847
wannazid <77951232+wannazid@users.noreply.github.com>
2022-12-20 18:42:53 +0700

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -22,7 +22,7 @@ Akan ada update atau versi setiap bulan bahkan kurang dari satu bulan.
 ''')
 st.subheader('Tentang Lainya')
 st.write('**Menu Tools Online** : ')
-list_menu = '<ul><li>DA PA Checker</li><li>Auto Wordpress Install</li><li>XMLRPC Result Check Login</li><li>Range IP Address</li><li>Grab Domain siterankdata.com</li><li>WordPress Install Checker</li><li>WordPress Setup Config Checker</li><li>Website Status Code</li></ul>'
+list_menu = '<ul><li>DA PA Checker</li><li>Auto Wordpress Install</li><li>XMLRPC Result Check Login</li><li>Range IP Address</li><li>Grab Domain siterankdata.com</li><li>WordPress Install Checker</li><li>WordPress Setup Config Checker</li><li>Website Status Code</li><li>Grab Domain in CubDomain.com</li><li>Domain to IP Address</li><li>Grab Domain WP by Theme</li><li>Grab Domain WP by Plugin</li><li>Grab Recent IP Address</li></ul>'
 st.markdown(list_menu, unsafe_allow_html=True)
 st.write('**Ketentuan Tools**  :')
 st.write('Tools ini gratis tidak memakan biaya sama sekali, kami tidak bertanggung jawab atas tindakan user, tools ini tidak menyimpan result atau hasil kalian karena setiap result akan langsung terhapus dalam waktu 30 detik, Anti Logger dan Still Update Tools ~')
@@ -30,6 +30,18 @@ st.subheader('Developer')
 images = Image.open('rn.jpg')
 st.image(images,width=250)
 st.write('**Ridwan N** | Coder Wibu | Penggiat IT')
+st.subheader('Updates 2.0')
+list_update = '''
+<ul>
+<li>Range IP > Perubahan result range lebih nyaman</li>
+<li>Cub Domain Grabber</li>
+<li>Domain to IP Address</li>
+<li>Grab Domain WP by Theme</li>
+<li>Grab Domain WP by Plugin</li>
+<li>Grab Recent IP Address</li>
+</ul>
+'''
+st.markdown(list_update,unsafe_allow_html=True)
 st.subheader('Lapor Bug / Error')
 st.write('Jika kalian mengalami error atau sebuah bug bisa langsung lapor ke link berikut atau ke menu.')
 report_bug = '''
